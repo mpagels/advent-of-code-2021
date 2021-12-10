6df5d494203f7d24c9ae99de1d8ac4a309b0b2ff
Martin <office@martinpagels.de>
2021-12-10 12:18:16 +0100

    add day 10 exercise 2

--- days/day10.js
+++ days/day10.js
@@ -62,6 +62,109 @@ async function main() {
     return (prev += illegalCalculation[cur])
   }, 0)
   console.log('What is the total syntax error score for those errors: ', result)
+
+  // exercise 2
+
+  // setup
+
+  const testDataAll = `[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]`
+
+  const data3 = testDataAll.split('\n').map((line) => line.split(''))
+  const unfinishedLines = []
+
+  const correspondingClosingChar = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+  }
+
+  const calculation = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+  }
+
+  const testData3 = '{<[[]]>}<{[{[{[]{()[[[]'.split('')
+  console.log(testData3)
+
+  // remove wrong lines
+
+  let isBreak = false
+  for (let i = 0; i < data.length; i++) {
+    const line = data[i]
+    stack.length = []
+    isBreak = false
+    for (let j = 0; j < line.length; j++) {
+      const char = line[j]
+      if (checkIsCloseChar(char)) {
+        const lastCharInStack = stack.pop()
+        if (lastCharInStack !== isCorrespondingClosingChar(char)) {
+          wrongChars.push(char)
+          stack.length = []
+          isBreak = true
+          break
+        }
+      } else {
+        stack.push(char)
+      }
+    }
+    if (isBreak === false) {
+      unfinishedLines.push(line)
+    }
+  }
+
+  // calculate closing logic
+  let stack2 = []
+  const closingStacks = []
+  let closingStack = []
+
+  for (let i = 0; i < unfinishedLines.length; i++) {
+    const line = unfinishedLines[i]
+    stack2 = []
+    for (let j = 0; j < line.length; j++) {
+      const char = line[j]
+      if (checkIsCloseChar(char)) {
+        stack2.pop()
+      } else {
+        stack2.push(char)
+      }
+    }
+
+    const length = stack2.length
+    closingStack = []
+    for (let i = 0; i < length; i++) {
+      const lastOpenChar = stack2.pop()
+      closingStack.push(correspondingClosingChar[lastOpenChar])
+    }
+
+    closingStacks.push(closingStack)
+  }
+
+  // calculate score
+  const results = []
+
+  closingStacks.forEach((closingStack) => {
+    const result = closingStack.reduce((prev, cur) => {
+      return prev * 5 + calculation[cur]
+    }, 0)
+    results.push(result)
+  })
+
+  const sortedResult = results.sort((a, b) => a - b)
+  const lengthOfResult = sortedResult.length
+  const middleIndex = lengthOfResult / 2 + 0.5
+  console.log('What is the middle score: ', sortedResult[middleIndex - 1])
 }
 
 main()
